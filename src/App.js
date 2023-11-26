350a984ac93e52c88afbc5b64a8f3c04c5eda46f
dmnhat91 <henry.duong02@gmail.com>
2023-11-26 14:39:17 +0700

    Add Layouts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import { Fragment } from "react"; // use Fragment to avoid render into a DOM element
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { publicRoutes } from "@/routes";
+import { DefaultLayout } from "@/components/Layout";
 
 function App() {
   return (
@@ -9,7 +11,25 @@ function App() {
           {publicRoutes.map((route, index) => {
             const Page = route.component;
 
-            return <Route key={index} path={route.path} element={<Page />} />;
+            let Layout = DefaultLayout;
+
+            if (route.layout) {
+              Layout = route.layout;
+            } else if (route.layout === null) {
+              Layout = Fragment;
+            }
+
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            );
           })}
         </Routes>
       </div>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,19 @@
+//Layouts
+import { HeaderOnly } from "@/components/Layout";
+
 import Home from "@/pages/Home";
 import Following from "@/pages/Following";
 import Profile from "@/pages/Profile";
+import Upload from "@/pages/Upload";
+import Search from "@/pages/Search";
 
 // Public routes: still see without logging in
 const publicRoutes = [
   { path: "/", component: Home },
   { path: "/following", component: Following },
   { path: "/profile", component: Profile },
+  { path: "/upload", component: Upload, layout: HeaderOnly },
+  { path: "/search", component: Search, layout: null },
 ];
 
 // Private routes: cannot see without login
