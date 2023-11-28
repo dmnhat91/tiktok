17780cf472b14e4c22594fadc13ef066281cdf8c
dmnhat91 <henry.duong02@gmail.com>
2023-11-28 22:40:01 +0700

    Build header layout

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,15 @@
+import classNames from "classnames/bind";
+import styles from "./Header.module.scss";
+
+// use classNames.bind to allow '-' in className (for example .post-item)
+const cx = classNames.bind(styles);
+
 function Header() {
-  return <h2>Header</h2>;
+  return (
+    <header className={cx("wrapper")}>
+      <div className={cx("inner")}>{/* logo */}</div>
+    </header>
+  );
 }
 
 export default Header;
