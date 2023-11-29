700d8cd2a060c2eefe20bf2843d4e3d1318134aa
dmnhat91 <henry.duong02@gmail.com>
2023-11-29 08:11:30 +0700

    Build Header bar part 1

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,8 @@
 import classNames from "classnames/bind";
 import styles from "./Header.module.scss";
+import images from "@/assets/images";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 // use classNames.bind to allow '-' in className (for example .post-item)
 const cx = classNames.bind(styles);
@@ -7,7 +10,22 @@ const cx = classNames.bind(styles);
 function Header() {
   return (
     <header className={cx("wrapper")}>
-      <div className={cx("inner")}>{/* logo */}</div>
+      <div className={cx("inner")}>
+        <div className={cx("logo")}>
+          <img src={images.logo} alt="Tiktok" />
+        </div>
+        <div className={cx("search")}>
+          <input placeholder="Search account and videos" spellCheck={false} />
+          <button className={cx("clear")}>
+            <FontAwesomeIcon icon={faCircleXmark} />
+          </button>
+          <FontAwesomeIcon className={cx("loading")} icon={faSpinner} />
+          <button className={cx("search-btn")}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </div>
+        <div className={cx("actions")}></div>
+      </div>
     </header>
   );
 }
