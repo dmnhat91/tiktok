88dbd3a40bf2300b25537a3f117063a2d2914a2e
dmnhat91 <henry.duong02@gmail.com>
2023-11-30 15:55:25 +0700

    Build up custom Button component

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 import classNames from "classnames/bind";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleXmark, faSpinner, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faCircleXmark, faSpinner, faMagnifyingGlass, faSignIn } from "@fortawesome/free-solid-svg-icons";
 import Tippy from "@tippyjs/react/headless";
 
+import Button from "@/components/Button";
 import { Wrapper as PopperWrapper } from "@/components/Popper";
 import styles from "./Header.module.scss";
 import images from "@/assets/images";
@@ -53,7 +54,12 @@ function Header() {
             </button>
           </div>
         </Tippy>
-        <div className={cx("actions")}></div>
+        <div className={cx("actions")}>
+          <Button text>Upload</Button>
+          <Button rounded outline>
+            Log In
+          </Button>
+        </div>
       </div>
     </header>
   );
